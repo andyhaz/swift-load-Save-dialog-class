aea5b1e9a4653e2ef622b6252d1a5736960db249
andrew <andyhaz1970@gmail.com>
2022-12-13 17:41:48 -0600

    Initial Commit

---
+++ rabbit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  rabbit
+//
+//  Created by andyhaz on 12/13/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ rabbitUITests/rabbitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  rabbitUITestsLaunchTests.swift
+//  rabbitUITests
+//
+//  Created by andyhaz on 12/13/22.
+//
+
+import XCTest
+
+final class rabbitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
