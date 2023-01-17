8714ee9046fbe26a5d71b9848cdc61d14b02f796
andrew <andyhaz1970@gmail.com>
2023-01-17 16:44:08 -0600

    Initial Commit

---
+++ load&Save Iamges/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  load&Save Iamges
+//
+//  Created by andyhaz on 1/17/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
