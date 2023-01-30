2a94a329807b6831237e26d0dfb8c182e85d67a0
andrew <andyhaz1970@gmail.com>
2023-01-30 00:35:25 -0600

    first commit

--- load&Save Iamges/ViewController.swift
+++ load&Save Iamges/ViewController.swift
@@ -8,19 +8,175 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    var myImage = NSImage()
 
+    //
+    @IBOutlet weak var imageWell: NSImageView!
+
+    @IBOutlet weak var myView: NSView!
+    
+    var myImageData:NSImage? = nil
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func loadAction(_ sender: Any) {
+        let lsm = LSManger()
+        lsm.fileLoadImage()
+        myImageData = lsm.fileImageData!
+        print(lsm.filePath as Any)
+        print("loadAction: \(String(describing: lsm.fileImageData))")
+    }
+    
+    @IBAction func saveAction(_ sender: Any) {
+        let lsm = LSManger()
+        lsm.fileSaveImage(imgData: myImageData!)
+        print(lsm.filePath as Any)
+        print("save\(myImage)")
+   // myurl:url?
+       // saveImageInDocumentDirectory(image: myImage, fileName: "test.png")
+        
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+   
 }
 
+class LSManger{
+    
+    var filePath:String? = nil
+    var fileImageData:NSImage? = nil
+    var fileTextData:String? = nil
+    var fileNameData:String? = nil
+    
+    func fileSaveImage(imgData:NSImage){
+      //  let image = NSImage()
+        saveDialog(titleBar: "save image")
+        fileImageData = imgData
+        if filePath != nil {
+           // let fileNameExt =
+           // let image = fileImageData
+           // var newfileName = filePath?.appending(".png")
+            saveImageInDocumentDirectory(fileName:fileNameData!, image: imgData)
+            print("save image")
+           // self.scaleImage(image: image!)
+        } else {
+            print(fileImageData as Any)
+        }
+    }
+    
+    func fileLoadImage(){
+        loadDialog(titleBar: "Load Image")
+        //load image
+        fileImageData = NSImage(contentsOfFile: filePath!)!
+        print("load image:\(String(describing: fileImageData))")
+    }
+    
+    func fileSaveText(){
+        saveDialog(titleBar: "save text")
+    }
+    
+    func loadTextFile(){
+        loadDialog(titleBar: "Load text")
+    }
+    
+    func fileSaveData(){
+        saveDialog(titleBar: "save file")
+    }
+    
+    func fileLoadData(){
+        loadDialog(titleBar: "load file")
+    }
+    
+    func saveDialog(titleBar:String){
+        let dialog = NSSavePanel();
+        dialog.title = titleBar;
+        
+        if (dialog.runModal() == NSApplication.ModalResponse.OK) {
+            let result = dialog.url // Pathname of the file
+            let nameResult = dialog.nameFieldStringValue
+            //print("name:\(nameResult)")
+                if (result != nil) {
+                   fileNameData = nameResult
+                   filePath = result!.path
+                   print("filepath:\(filePath!)")
+                }
+            } else {
+                // User clicked on "Cancel"
+                return
+            }
+     /*let chosenFile = dialog.url
+        if chosenFile != nil {
+          //  let image = NSImage(contentsOf: chosenFile!)
+           // self.scaleImage(image: image!)
+        }*/
+    }//
+    
+    func loadDialog(titleBar:String){
+        let dialog = NSOpenPanel();
+            dialog.title                   = titleBar;
+            dialog.showsResizeIndicator    = true;
+            dialog.showsHiddenFiles        = false;
+            dialog.canChooseDirectories    = true;
+            dialog.canCreateDirectories    = true;
+            dialog.allowsMultipleSelection = false;
+        if (dialog.runModal() == NSApplication.ModalResponse.OK) {
+            let result = dialog.url // Pathname of the file
+                if (result != nil) {
+                   filePath = result!.path
+                }
+            } else {
+                // User clicked on "Cancel"
+                return
+            }
+    }
+    
+    public func saveImageInDocumentDirectory(fileName: String, image: NSImage) {
+        let newfileName = fileName.appending(".png")
+        print("newfilename:\(String(describing: newfileName))")
+        
+        let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!;
+        print("documentURL:\(documentsUrl)")
+        
+       // let fileURL = documentsUrl.appendingPathComponent(newfileName)
+        let fileURL = URL(fileURLWithPath: (filePath?.appending(".png"))!)
+
+        print("fileurl:\(fileURL)")
+
+           // let imageData = image!.pngData()! as NSData
+            if let imageData = image.pngData {
+                try? imageData.write(to: fileURL, options: .atomic)
+                
+              //  print(imageData)
+              //  return fileURL
+               // print(fileURL)
+            }
+           // return nil
+        }
+}
+extension NSImage {
+    var pngData: Data? {
+        guard let tiffRepresentation = tiffRepresentation, let bitmapImage = NSBitmapImageRep(data: tiffRepresentation)
+        else { return nil }
+        return bitmapImage.representation(using: .png, properties: [:])
+    }
+    
+    func pngWrite(to url: URL, options: Data.WritingOptions = .atomic) -> Bool {
+        do {
+            try pngData?.write(to: url, options: options)
+            return true
+        } catch {
+            print(error)
+            return false
+        }
+    }
+}
