6ad580e23d28bee65e28778145d785ce40f59f13
andrew <andyhaz1970@gmail.com>
2023-02-07 13:59:33 -0600

    add created directory

--- load&Save Iamges/LSManger.swift
+++ load&Save Iamges/LSManger.swift
@@ -140,6 +140,20 @@ class LSManger{
         return textData
     }
     
+    public func fileCreatDir(dirName:String){
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let documentsDirectory = paths[0]
+        let docURL = URL(string: documentsDirectory)!
+        let dataPath = docURL.appendingPathComponent(dirName)
+        if !FileManager.default.fileExists(atPath: dataPath.path) {
+            do {
+                try FileManager.default.createDirectory(atPath: dataPath.path, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
 }
 
 extension NSImage {
